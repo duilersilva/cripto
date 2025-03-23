9ca51d24c2197ba6e86e0af8d5cd8cba2aa5f0a3
duilersilva <duilerdd@gmail.com>
2025-03-23 14:13:34 -0300

    projeto finalizado

--- src/cripto.java
+++ src/cripto.java
@@ -5,14 +5,16 @@ public class cripto {
                 // inicio do programa coletar variavel que será criptografada
                 // exibir uma caixa de dialogo para o usuario inserir uma string
                 String string_input = JOptionPane.showInputDialog(null,
-                                "<html><font color='blue' size='5'>Entre com uma String:</font></html>",
+                                "<html><font color='blue'>Entre com uma String:</font>",
                                 "Insira Letras para Criptografar", JOptionPane.DEFAULT_OPTION);
+
                 // exibicao String digitada
                 JOptionPane.showMessageDialog(null,
-                                "A String digitada foi:  " + string_input,
+                                "<html><font color='blue'>A String digitada foi :</font>" + " " + string_input,
                                 "O que será Criptografado", JOptionPane.DEFAULT_OPTION);
 
                 // converter a string para um array de caracteres
+
                 char[] caracteres = string_input.toCharArray();
 
                 // StringBuilder para armazenar a mensagem criptografada
@@ -25,15 +27,18 @@ public class cripto {
 
                         // exibir o caractere e o seu valor em ASCII
                         JOptionPane.showMessageDialog(null,
-                                        "Caractere : " + c + " - Valor decimal do caractere na tabela ASCII: "
+                                        "<html><font color = 'green'><n>Caracatere : </n></font>" + c
+                                                        + "<font color = 'blue'><br><i><n>Valor tabela ASCII :</i></n></font>"
                                                         + ascii_original,
                                         "Valor Original", JOptionPane.NO_OPTION);
+
                         // exibir o caractere criptografado
                         JOptionPane.showMessageDialog(null,
-                                        "Caractere: " + c
-                                                        + " - Valor decimal do caractere na tabela ASCII + 10 unidades: "
+                                        "<html><font color = 'green'><n>Caracatere : </n></font>" + c
+                                                        + "<font color = 'blue'><br><i><n>Valor tabela ASCII + 10 unidades : </i></n></font>"
                                                         + ascii_criptografado,
                                         "Valor Criptografado", JOptionPane.NO_OPTION);
+
                         // converter o novo valor ascii para caractere
                         char string_criptografada = (char) (ascii_criptografado);
 
@@ -43,15 +48,18 @@ public class cripto {
 
                 // exibir a mensagem criptografada
                 JOptionPane.showMessageDialog(null,
-                                "A mensagem criptografada é: " + mensagem_criptografada.toString());
+                                "<html><font color = 'red' size ='5'><n> Mensagem Criptografada : </n><br></font>"
+                                                + mensagem_criptografada.toString(),
+                                "Mensagem Criptografada", JOptionPane.NO_OPTION);
 
                 // avaliação do projeto
                 int escolha = JOptionPane.showConfirmDialog(null,
-                                "Você gostou do projeto ?", "Avaliação", JOptionPane.YES_NO_OPTION);
+                                "Você gostou do projeto ?", "Avaliação",
+                                JOptionPane.YES_NO_OPTION);
                 if (escolha == JOptionPane.YES_OPTION) {
 
                         JOptionPane.showMessageDialog(null,
-                                        "<html><font color='red'>Obrigado pela avaliação</font></html>",
+                                        "<html><font color='green'>Obrigado pela avaliação</font>",
                                         "Avaliação", JOptionPane.NO_OPTION);
 
                 } else {
@@ -62,16 +70,3 @@ public class cripto {
 
         }
 }
-
-/*
- * JOptionPane.showMessageDialog(null,
- * "<html><i>Primeira Linha</i><br>Segunda linha<br>Terceira linha</html>",
- * "Quebra de Linha", JOptionPane.NO_OPTION);
- * JOptionPane.showMessageDialog(null,
- * "<html><font color='red'>Texto Vermelho</font></html>",
- * "Cor Personalizada", JOptionPane.NO_OPTION);
- * 
- * JOptionPane.showMessageDialog(null,
- * "<html><font size='56'>Texto Grande</font></html>",
- * "Tamanho de Fonte", JOptionPane.NO_OPTION);
- */
